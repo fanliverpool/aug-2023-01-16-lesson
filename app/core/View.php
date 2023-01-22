0f6a377092c5a90caf2c00334f083e0892927d91
vladislav <vladislav.nikitin.home@gmail.com>
2023-01-22 19:53:19 +0200

    realisation class

--- app/core/View.php
+++ app/core/View.php
@@ -4,5 +4,37 @@ namespace app\core;
 
 class View
 {
+    const VIEWS_DIR = '..'.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.'views';
 
+    protected $template = 'main';
+
+    protected $page;
+
+    public function __construct(string $template = null)
+    {
+        if (!is_null($template)) {
+            $this->template = $template;
+        }
+    }
+
+    public function render(string $page, $params = []) {
+        $this->page = $page;
+        include_once $this->getTemplatePath();
+    }
+
+    /**
+     * return formatted path to templates directory
+     * @return string
+     */
+    private function getTemplatePath() {
+        return self::VIEWS_DIR.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR.$this->template.'.php';
+    }
+
+    /**
+     * return formatted path to pages directory
+     * @return string
+     */
+    private function getPagePath() {
+        return self::VIEWS_DIR.DIRECTORY_SEPARATOR.'pages'.DIRECTORY_SEPARATOR.$this->page.'.php';
+    }
 }
